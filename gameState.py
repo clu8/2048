7df3b98a5825b74872739c60abec2db4c80aa05b
Stephen Ou <me@stephenou.com>
2015-11-07 21:19:30 -0800

    Add isWin and isLose

--- gameState.py
+++ gameState.py
@@ -4,8 +4,25 @@ class GameState2048:
 	scores, and more.
 	"""
 
+	WINNING_TILE = 2048
+
 	def __init__(self, size):
-		self.board = [[0 for _ in range(size)] for _ in range(size)]
+		self.board = [[0 for _ in range(size)] for _ in range(size)] # store the value at each tile, 0 means empty
+		self.score = 0
+
+	def isWin():
+		for row in self.board:
+			for tile in row:
+				if tile == 2048:
+					return True
+		return False
+
+	def isLose():
+		for row in self.board:
+			for tile in row:
+				if tile > 0:
+					return False
+		return True
 
 game = GameState2048(4)
 assert game.board == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
