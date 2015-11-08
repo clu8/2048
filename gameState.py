809689f4419914cf8bbec801f3e4dd67805c0e04
Stephen Ou <me@stephenou.com>
2015-11-07 21:40:40 -0800

    Add more asserts

--- gameState.py
+++ gameState.py
@@ -1,13 +1,8 @@
-from enum import Enum
+class Move:
+	left, up, right, down = range(4)
 
-class Move(Enum):
-    left = 1
-    up = 2
-    right = 3
-    down = 4
-
-    def getAllMoves():
-    	return [left, up, right, down]
+	def getAllMoves(self):
+		return [self.left, self.up, self.right, self.down]
 
 class GameState2048:
 	"""
@@ -19,22 +14,23 @@ class GameState2048:
 	WINNING_TILE = 2048
 
 	def __init__(self):
-		self.board = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)] # store the value at each tile, 0 means empty
+		self.board = [[0 for _ in range(self.BOARD_SIZE)] for _ in range(self.BOARD_SIZE)] # store the value at each tile, 0 means empty
 		self.score = 0
+		self.moves = Move()
 
 	def getLegalAction(self, agentIndex = 0):
 		assert agentIndex == 0 or agentIndex == 1
 		if self.isWin() or self.isLose():
 			return []
 		if agentIndex == 0:  # human player
-		  return Move.getAllMoves()
+		  return self.moves.getAllMoves()
 		else:
-		  return [(row, col) for col in range(BOARD_SIZE) for row in range(BOARD_SIZE) if self.isTileEmpty(row, col)]
+		  return [(row, col) for col in range(self.BOARD_SIZE) for row in range(self.BOARD_SIZE) if self.isTileEmpty(row, col)]
 
 	def isWin(self):
 		for row in self.board:
 			for tile in row:
-				if tile == WINNING_TILE:
+				if tile == self.WINNING_TILE:
 					return True
 		return False
 
@@ -45,10 +41,10 @@ class GameState2048:
 					return False
 		return True
 
-	def isTileEmpty(row, col):
+	def isTileEmpty(self, row, col):
 		return self.board[row][col] == 0
 
-	def setValue(row, col, value):
+	def setValue(self, row, col, value):
 		assert value ^ (value - 1) # value should be power of 2
 		self.board[row][col] = value
 
@@ -56,11 +52,8 @@ game = GameState2048()
 
 assert game.board == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
-assert game.getLegalAction(0) == [Move.left, Move.up, Move.right, Move.down]
-
-row, col = game.getLegalAction(1)
-assert row >= 0 and row < game.BOARD_SIZE
-assert col >= 0 and col < game.BOARD_SIZE
+assert game.getLegalAction(0) == [game.moves.left, game.moves.up, game.moves.right, game.moves.down]
+assert len(game.getLegalAction(1)) == game.BOARD_SIZE ** 2
 
 assert game.isWin() == False
 assert game.isLose() == False
