e512244338b059fa65b77702a3cb6d0fd26f1f0b
Stephen Ou <me@stephenou.com>
2015-11-07 21:33:36 -0800

    Add getLegalAction, isTileEmpty, and setValue

--- gameState.py
+++ gameState.py
@@ -1,28 +1,66 @@
+from enum import Enum
+
+class Move(Enum):
+    left = 1
+    up = 2
+    right = 3
+    down = 4
+
+    def getAllMoves():
+    	return [left, up, right, down]
+
 class GameState2048:
 	"""
 	A GameState2048 specifies the full game state, including the tile values,
 	scores, and more.
 	"""
 
+	BOARD_SIZE = 4
 	WINNING_TILE = 2048
 
-	def __init__(self, size):
-		self.board = [[0 for _ in range(size)] for _ in range(size)] # store the value at each tile, 0 means empty
+	def __init__(self):
+		self.board = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)] # store the value at each tile, 0 means empty
 		self.score = 0
 
-	def isWin():
+	def getLegalAction(self, agentIndex = 0):
+		assert agentIndex == 0 or agentIndex == 1
+		if self.isWin() or self.isLose():
+			return []
+		if agentIndex == 0:  # human player
+		  return Move.getAllMoves()
+		else:
+		  return [(row, col) for col in range(BOARD_SIZE) for row in range(BOARD_SIZE) if self.isTileEmpty(row, col)]
+
+	def isWin(self):
 		for row in self.board:
 			for tile in row:
-				if tile == 2048:
+				if tile == WINNING_TILE:
 					return True
 		return False
 
-	def isLose():
+	def isLose(self):
 		for row in self.board:
 			for tile in row:
-				if tile > 0:
+				if tile == 0:
 					return False
 		return True
 
-game = GameState2048(4)
+	def isTileEmpty(row, col):
+		return self.board[row][col] == 0
+
+	def setValue(row, col, value):
+		assert value ^ (value - 1) # value should be power of 2
+		self.board[row][col] = value
+
+game = GameState2048()
+
 assert game.board == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+assert game.getLegalAction(0) == [Move.left, Move.up, Move.right, Move.down]
+
+row, col = game.getLegalAction(1)
+assert row >= 0 and row < game.BOARD_SIZE
+assert col >= 0 and col < game.BOARD_SIZE
+
+assert game.isWin() == False
+assert game.isLose() == False
